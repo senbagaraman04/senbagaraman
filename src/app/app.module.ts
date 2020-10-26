f85ae41c20408f31e1549b2438c05b4295dbef76
Senbagaraman <senbagaraman.manoharan@siemens.com>
2020-10-26 09:13:23 +0530

    fixed space

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatToolbarModule,MatIconModule} from '@angular/material';
+import {MatToolbarModule, MatIconModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
